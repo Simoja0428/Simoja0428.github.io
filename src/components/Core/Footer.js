cc422544673d28bf5866f76383af21dfa06c061f
Jacob Simonek <jacob@swimcloud.com>
2022-12-17 22:49:32 -0600

    Remove Apt #

--- src/components/Core/Footer.js
+++ src/components/Core/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
                 <div className="location">
                     <FaHome size={20} style={{ color:"#fff", marginRight: "2rem"}} />
                     <div>
-                        <p>8545 22nd Ave Apt 1, Kenosha, WI. 53143</p>
+                        <p>8545 22nd Ave, Kenosha, WI. 53143</p>
                     </div>
                 </div>
                 <div className = "phone">
